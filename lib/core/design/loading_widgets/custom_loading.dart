dc89b8e6841b8e60a0072119c92f0b6dcdbd8dbf
hussein <husseinpotter19@gmail.com>
2024-09-24 16:03:23 +0300

    make transparent option

--- lib/core/design/loading_widgets/custom_loading.dart
+++ lib/core/design/loading_widgets/custom_loading.dart
@@ -4,14 +4,16 @@ import 'package:evcs/core/utils/screen_utils/device_utils.dart';
 import 'package:flutter/material.dart';
 
 class CustomLoading extends StatelessWidget {
-  const CustomLoading({super.key});
-
+  const CustomLoading({super.key, this.isTrasparent = false});
+  final bool isTrasparent;
   @override
   Widget build(BuildContext context) {
     return Container(
       width: ScreenHelper.width,
       height: ScreenHelper.height,
-      color: DIManager.findCC().greyLightTextColor.withOpacity(0.5),
+      color: isTrasparent
+          ? DIManager.findCC().transparent
+          : DIManager.findCC().greyLightTextColor.withOpacity(0.5),
       child: Center(
         child: SizedBox(
           width: ScreenHelper.fromWidth(30),
